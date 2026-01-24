20f11fb8f97e824f1ec1907fd32a91a8a3bd8c75
Thiago <thiagomarcial1109@gmail.com>
2026-01-23 23:51:41 -0300

    Added menu section

---
+++ src/data/menuData.js
@@ -0,0 +1,52 @@
+const route = "/menu-coffee-pictures/"
+
+export const menuItems =[
+    {
+        name:"Americano",
+        image: `${route}americano.png`,
+        alt: "Americano in a cup on a table",
+        description:"A classic black coffee made with a shot of espresso and hot water.",
+        price: "2.50",
+    },
+
+    {
+        name:"Cappuccino",
+        image: `${route}capuccino.png`,
+        alt: "Cappuccino in a cup on a table",
+        description:"A rich and creamy coffee drink made with espresso and steamed milk.",
+        price: "3.00",
+    },
+    
+    {
+        name:"Latte",
+        image: `${route}latte.png`,
+        alt: "Latte in a cup on a table",
+        description:"Equal parts espresso, steamed milk, and foam, offering a balanced and frothy texture.",
+        price: "4.00",
+    },
+
+    {
+        name:"Espresso",
+        image: `${route}espesso.png`,
+        alt: "Espresso in a cup on a table",
+        description:"A strong and concentrated shot of pure espresso.",
+        price: "2.50",
+    },
+
+    {
+        name:"Cortado",
+        image: `${route}cortado.png`,
+        alt: "Cortado in a cup on a table",
+        description:"Espresso cut with a small amount of steamed milk for a bold yet balanced flavor.",
+        price: "3.00",
+    },
+
+    {
+        name:"Iced Coffee",
+        image: `${route}helado.png`,
+        alt: "Iced Coffee in a cup on a table",
+        description:"Chilled coffee served over ice with a touch of vanilla syrup.",
+        price: "3.50",
+    }
+
+]
