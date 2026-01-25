d78a859b4f4eed3bfd2528abfef1c2eb2cc5f9a5
Thiago <thiagomarcial1109@gmail.com>
2026-01-25 17:46:47 -0300

    Added github actions workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://thiagodev1196.github.io',
+  base: '/Furina-de-Fontaine-A',
+});
