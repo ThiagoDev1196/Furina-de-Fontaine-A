34fd2bd2079395df9785e8adbded8647e1c9a167
Thiago <thiagomarcial1109@gmail.com>
2026-01-25 17:56:32 -0300

    Adding base in data for menu pictures

--- src/data/menuData.js
+++ src/data/menuData.js
@@ -1,9 +1,9 @@
-const route = "/menu-coffee-pictures/"
+const base = import.meta.env.BASE_URL
 
 export const menuItems =[
     {
         name:"Americano",
-        image: `${route}americano.png`,
+        image: `${base}/menu-coffee-pictures/americano.png`,
         alt: "Americano in a cup on a table",
         description:"A classic black coffee made with a shot of espresso and hot water.",
         price: "2.50",
@@ -11,7 +11,7 @@ export const menuItems =[
 
     {
         name:"Cappuccino",
-        image: `${route}capuccino.png`,
+        image: `${base}/menu-coffee-pictures/capuccino.png`,
         alt: "Cappuccino in a cup on a table",
         description:"A rich and creamy coffee drink made with espresso and steamed milk.",
         price: "3.00",
@@ -19,7 +19,7 @@ export const menuItems =[
     
     {
         name:"Latte",
-        image: `${route}latte.png`,
+        image: `${base}/menu-coffee-pictures/latte.png`,
         alt: "Latte in a cup on a table",
         description:"Equal parts espresso, steamed milk, and foam, offering a balanced and frothy texture.",
         price: "4.00",
@@ -27,7 +27,7 @@ export const menuItems =[
 
     {
         name:"Espresso",
-        image: `${route}espesso.png`,
+        image: `${base}/menu-coffee-pictures/espesso.png`,
         alt: "Espresso in a cup on a table",
         description:"A strong and concentrated shot of pure espresso.",
         price: "2.50",
@@ -35,7 +35,7 @@ export const menuItems =[
 
     {
         name:"Cortado",
-        image: `${route}cortado.png`,
+        image: `${base}/menu-coffee-pictures/cortado.png`,
         alt: "Cortado in a cup on a table",
         description:"Espresso cut with a small amount of steamed milk for a bold yet balanced flavor.",
         price: "3.00",
@@ -43,7 +43,7 @@ export const menuItems =[
 
     {
         name:"Iced Coffee",
-        image: `${route}helado.png`,
+        image: `${base}/menu-coffee-pictures/helado.png`,
         alt: "Iced Coffee in a cup on a table",
         description:"Chilled coffee served over ice with a touch of vanilla syrup.",
         price: "3.50",
